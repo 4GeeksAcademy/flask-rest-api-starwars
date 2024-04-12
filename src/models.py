8b49b8870d5a758c3e24bd8832bcbce21650112d
terry howell <63936327+terrylh22@users.noreply.github.com>
2024-04-12 03:15:28 +0000

    created people planets and user db tables and endpoints

--- src/models.py
+++ src/models.py
@@ -3,10 +3,13 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    first_name = db.Column(db.String(30), unique=False, nullable=False)
+    last_name = db.Column(db.String(30), unique=False, nullable=False)
+    username = db.Column(db.String(40), unique=True, nullable=False)
+    
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -15,5 +18,46 @@ class User(db.Model):
         return {
             "id": self.id,
             "email": self.email,
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "username": self.username
+            # do not serialize the password, its a security breach
+        }
+
+class Planets(db.Model):
+    planet_id = db.Column(db.Integer, primary_key=True)
+    planet_name = db.Column(db.String(100))
+    diameter = db.Column(db.Integer)
+    temperature = db.Column(db.Integer)
+    description = db.Column(db.String(400))
+
+    def __repr__(self):
+        return '<Planets %r>' % self.planet_id
+
+    def serialize(self):
+        return {
+            "planet_id": self.planet_id,
+            "planet_name": self.planet_name,
+            "diameter": self.diameter,
+            "temperature": self.temperature,
+            "description": self.description
+            # do not serialize the password, its a security breach
+        }
+    
+class People(db.Model):
+    person_id = db.Column(db.Integer, primary_key=True)
+    person_name = db.Column(db.String(250))
+    weight = db.Column(db.Integer)
+    height = db.Column(db.Integer)
+
+    def __repr__(self):
+        return '<People %r>' % self.person_id
+
+    def serialize(self):
+        return {
+            "person_id": self.person_id,
+            "person_name": self.person_name,
+            "weight": self.weight,
+            "height": self.height,
             # do not serialize the password, its a security breach
         }
